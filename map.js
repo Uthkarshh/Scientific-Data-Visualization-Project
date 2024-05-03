f1b852d255fb3e878c37eeddf6e66e9acb27d2d3
Uthkarsh Reddy Junuthula <106112065+Uthkarshh@users.noreply.github.com>
2024-05-03 07:47:49 -0500

    Update map.js

--- map.js
+++ map.js
@@ -6,7 +6,7 @@ const projection = d3.geoNaturalEarth1().scale(width / 6).translate([width / 2, 
 const path = d3.geoPath().projection(projection);
 const colorScale = d3.scaleLog().range(['#FFFB46', '#542344']).interpolate(d3.interpolateHcl);
 
-d3.json('https://raw.githubusercontent.com/Uthkarshh/Task-8/main/world.GeoJSON').then(function (world) {
+d3.json('https://raw.githubusercontent.com/Uthkarshh/Scientific-Data-Visualization-Project/main/world.GeoJSON').then(function (world) {
     renderMap(world);
     d3.select('#year-select').on('change', function () { updateMap(world); });
     d3.select('#metric-select').on('change', function () { updateMap(world); });
@@ -24,7 +24,7 @@ function renderMap(world) {
 function updateMap(world) {
     const year = d3.select('#year-select').node().value;
     const metric = d3.select('#metric-select').node().value;
-    d3.csv('https://raw.githubusercontent.com/Uthkarshh/Task-8/main/Covid%20Data.csv', function (d) {
+    d3.csv('https://raw.githubusercontent.com/Uthkarshh/Scientific-Data-Visualization-Project/main/Covid%20Data.csv', function (d) {
         const date = new Date(d.date);
         return date.getFullYear().toString() === year ? d : null;
     }).then(function (data) {
